4a6ec9d840beee1af04f52b7c343668011a8d1bd
Bob Remeika <bob@ragie.ai>
2025-03-17 16:43:01 -0700

    Prefer underscores for tool names

--- src/index.ts
+++ src/index.ts
@@ -53,7 +53,7 @@ if (options.description) {
 const description = options.description || defaultDescription;
 
 server.tool(
-  "knowledge-base-retrieve",
+  "knowledge_base_retrieve",
   description,
   {
     query: z
